449c25be8756e8eb5cb7c37b2dafe8ddea7fdf1f
Romanweiss <romanweiblearning@gmail.com>
2023-12-27 19:26:27 +0300

    import model to admin

--- PostUrl/appSite/admin.py
+++ PostUrl/appSite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import FormUser
 
-# Register your models here.
+@admin.register(FormUser)
+class FormUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'age'] # отображение полей в админке
+    list_display_links = ['name', 'email'] # для кликабельности в админке
+    list_filter = ['age'] # для фильтрации в админке
